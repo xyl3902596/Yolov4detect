ffd5dd88d4959db21e15c9be00a3862a2b2e438a
xyl3902596 <XiYunlong111@outlook.com>
2020-05-28 12:47:43 +0800

    first_commit

---
+++ cmake-build-debug/CMakeFiles/yolov4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/yolov4.dir/main.cpp.o"
+  "yolov4.pdb"
+  "yolov4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/yolov4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
